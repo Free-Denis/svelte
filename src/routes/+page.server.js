9454e8cc07100b8d22077878b088bdf07d6c079d
Free-Denis <147907574+Free-Denis@users.noreply.github.com>
2025-05-18 14:43:53 +0500

    Update +page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,11 @@
+/** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    const res = await fetch("https://oavha4ppbd3sfhp7.public.blob.vercel-storage.com/posts.json");
+    const res = await fetch('https://oavha4ppbd3sfhp7.public.blob.vercel-storage.com/posts.json');
+    if (!res.ok) {
+        console.error('Ошибка при загрузке данных:', res.statusText);
+        return { summaries: [] };
+    }
+
     const posts = await res.json();
 
     return {
