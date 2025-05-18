8ccdd1d4a2db7b2c239455508f1db14a10311ff7
Free-Denis <147907574+Free-Denis@users.noreply.github.com>
2025-05-18 14:17:31 +0500

    Update +page.server.js

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,9 +1,15 @@
-import { posts } from '../data.js';
+import { fetchPosts } from '$lib/fetchPosts.js';
 
-export function load({ params }) {
+export async function load({ params }) {
+    const posts = await fetchPosts();
     const post = posts.find(p => p.slug === params.slug);
-    
-    if (!post) return { status: 404 };
-    
+
+    if (!post) {
+        return {
+            status: 404,
+            error: new Error('Пост не найден')
+        };
+    }
+
     return { post };
 }
