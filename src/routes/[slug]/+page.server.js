ad0ba1ba0263e8e0a9e756298200da75cc3ee898
Free-Denis <147907574+Free-Denis@users.noreply.github.com>
2025-05-18 14:45:08 +0500

    Update +page.server.js

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,14 +1,15 @@
+/** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-    const res = await fetch("https://oavha4ppbd3sfhp7.public.blob.vercel-storage.com/posts.json");
-    const posts = await res.json();
-
-    const post = posts.find(p => p.slug === params.slug);
-
-    if (!post) {
-        return {
-            status: 404
-        };
+    const res = await fetch('https://oavha4ppbd3sfhp7.public.blob.vercel-storage.com/posts.json');
+    if (!res.ok) {
+        console.error('Ошибка при загрузке постов:', res.statusText);
+        return { status: 500 };
     }
+
+    const posts = await res.json();
+    const post = posts.find((p) => p.slug === params.slug);
+
+    if (!post) return { status: 404 };
 
     return { post };
 }
